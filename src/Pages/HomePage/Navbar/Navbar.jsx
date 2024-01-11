6987468a8f02ecadf59d4b776356b470a7b43f7d
afifa <2023afifa@gmail.com>
2024-01-12 00:02:30 +0600

    Dropdown section is done

--- src/Pages/HomePage/Navbar/Navbar.jsx
+++ src/Pages/HomePage/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
                         <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
                         </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-1 z-[1] p-1 shadow bg-base-100 rounded-box w-28">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content right-3 z-[1] p-1 shadow bg-base-100 rounded-box w-28">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="about" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">About</Link></li>
                             <li><Link to="project" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">Projects</Link></li>
