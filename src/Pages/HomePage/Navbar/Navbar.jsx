91602baf859d60818ce38cf2c1c865ccb5aee2ab
afifa <2023afifa@gmail.com>
2024-01-11 21:34:14 +0600

    Medium screen responsive done

--- src/Pages/HomePage/Navbar/Navbar.jsx
+++ src/Pages/HomePage/Navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="navbar-end md:hidden border-2">
+                <div className="navbar-end md:hidden">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
