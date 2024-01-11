838e0115a778bff8d8e1b81a720e63fe8db41f34
afifa <2023afifa@gmail.com>
2024-01-11 23:52:23 +0600

    Navbar for mobile screen responsive done

--- src/Pages/HomePage/Navbar/Navbar.jsx
+++ src/Pages/HomePage/Navbar/Navbar.jsx
@@ -6,14 +6,17 @@ const Navbar = () => {
     return (
         <div>
             <div className="navbar md:px-5 lg:px-10 bg-slate-50 fixed z-10">
-                <div className="navbar-start">
-                    <div className="flex-1 border-2">
+                <div className="md:navbar-start">
+                    <div className="flex-1">
                         <h2 className="text-3xl font-bold">My Portfolio</h2>
                     </div>
                 </div>
-                <div className="navbar-end hidden md:flex">
-                    <div className="flex-none navLink">
-                        <ul className="menu menu-horizontal px-1 lg:text-lg font-medium">
+                <div className="md:hidden flex flex-grow justify-end">
+                    <div className="dropdown navLink">
+                        <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-28">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="about" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">About</Link></li>
                             <li><Link to="project" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">Projects</Link></li>
@@ -22,12 +25,9 @@ const Navbar = () => {
                         </ul>
                     </div>
                 </div>
-                <div className="navbar-end md:hidden">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-                        </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                <div className="navbar-end hidden md:flex">
+                    <div className="flex-none navLink">
+                        <ul className="menu menu-horizontal px-1 lg:text-lg font-medium">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="about" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">About</Link></li>
                             <li><Link to="project" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">Projects</Link></li>
