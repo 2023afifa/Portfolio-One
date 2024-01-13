686a726d3640931b9351dceabf0c2ffdc3756ecc
afifa <2023afifa@gmail.com>
2024-01-13 19:23:46 +0600

    About section alignment changed for medium screen

--- src/Pages/HomePage/About/About.jsx
+++ src/Pages/HomePage/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             I believe in the power of thoughtful design and seamless functionality, and I'm on a quest to merge creativity with technology. Whether crafting intuitive user interfaces or diving into the backend intricacies, I find joy in the entire web development spectrum.
 
             Currently on the lookout for new opportunities, I am excited about the prospect of contributing to innovative projects and collaborating with like-minded individuals. Join me on this journey through my projects and experiences, and let's create something extraordinary together!</p>
-            <div className="mx-10 md:mx-8 lg:mx-3 my-20 lg:flex lg:justify-around lg:items-center">
+            <div className="mx-10 md:mx-5 lg:mx-3 my-20 md:flex md:justify-around md:items-center">
                 <h2 className="text-3xl font-semibold">Graduated From</h2>
                 <div>
                     <p className="text-2xl font-medium">North South University</p>
