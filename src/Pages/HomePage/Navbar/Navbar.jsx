db8c6f84ee6850a6573cea54dfae842383a2f8af
afifa <2023afifa@gmail.com>
2024-03-06 17:59:06 +0600

    Link changed for resume

--- src/Pages/HomePage/Navbar/Navbar.jsx
+++ src/Pages/HomePage/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
                             <li><Link to="about" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">About</Link></li>
                             <li><Link to="project" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">Projects</Link></li>
                             <li><Link to="contact" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">Contact</Link></li>
-                            <li><a href="https://drive.google.com/uc?export=download&id=1CJjiKKOBgbtsU70adih7l3mPUAc03QSO" download>Resume</a></li>
+                            <li><a href="https://drive.google.com/uc?export=download&id=1de86_vdUeL-Pz-RbK9APQzxpxETHh83Y" download>Resume</a></li>
                         </ul>
                     </div>
                 </div>
@@ -32,7 +32,7 @@ const Navbar = () => {
                             <li><Link to="about" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">About</Link></li>
                             <li><Link to="project" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">Projects</Link></li>
                             <li><Link to="contact" spy={true} smooth={true} offset={-80} duration={500} activeClass="active">Contact</Link></li>
-                            <li><a href="https://drive.google.com/uc?export=download&id=1CJjiKKOBgbtsU70adih7l3mPUAc03QSO" download>Resume</a></li>
+                            <li><a href="https://drive.google.com/uc?export=download&id=1de86_vdUeL-Pz-RbK9APQzxpxETHh83Y" download>Resume</a></li>
                         </ul>
                     </div>
                 </div>
