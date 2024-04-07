33504fc3f7fa28de80b130806d9ccbcfb904fb74
afifa <2023afifa@gmail.com>
2024-04-08 00:31:30 +0600

    Skills section aos added

--- src/Pages/HomePage/Skill/Skill.jsx
+++ src/Pages/HomePage/Skill/Skill.jsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
 const Skill = () => {
+    useEffect(() => {
+        AOS.init();
+    }, []);
+
+
     return (
-        <div className="pt-16 md:px-10 lg:px-60 bg-gray-50">
+        <div className="pt-16 md:px-10 lg:px-60 bg-gray-50" data-aos="flip-left" data-aos-duration="2000">
             <h2 className="text-3xl text-center font-semibold mb-3">My Skills</h2>
             <div className="bg-cyan-500 h-2 w-12 rounded-2xl mx-auto"></div>
             <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10 justify-center my-10">
