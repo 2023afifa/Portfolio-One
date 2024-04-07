9b1b8fb2afd2c4cf864448e77f2113db02c6596f
afifa <2023afifa@gmail.com>
2024-04-07 23:42:13 +0600

    Banner text style updated

--- src/Pages/HomePage/Banner/Banner.jsx
+++ src/Pages/HomePage/Banner/Banner.jsx
@@ -1,21 +1,21 @@
+import Typewriter from 'typewriter-effect';
+
 const Banner = () => {
     return (
         <div>
-            {/* <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://i.ibb.co/h19hPSN/4890914.jpg)' }}>
-                <div className="hero-overlay bg-opacity-20"></div>
-                <div className="hero-content text-center">
-                    <div className="lg:max-w-xl">
-                        <h1 className="mb-5 text-5xl font-bold">Hi, I'm Afifa</h1>
-                        <p className="mb-5 text-xl">A dedicated frontend developer with a passion for crafting engaging user experiences through the art of coding</p>
-                    </div>
-                </div>
-            </div> */}
-
-            <div className="hero min-h-screen bg-slate-100">
+            <div className="hero min-h-screen bg-slate-200">
                 <div className="flex flex-col lg:flex-row-reverse justify-around items-center w-full">
                     <img className="w-[500px] rounded-md" src="https://i.ibb.co/bmJ5LKW/Banner-gif.gif" />
-                    <div className="lg:max-w-xl">
-                        <h1 className="mb-5 text-5xl font-bold">Hi, I'm Afifa</h1>
+                    <div className="lg:max-w-2xl">
+                        <h1 className="mb-5 font-bold">
+                            <span className='text-4xl'>Hi, I'm</span>
+                            <span className='text-5xl'><Typewriter
+                                options={{
+                                    strings: ['Afifa', 'A Web Developer'],
+                                    autoStart: true,
+                                    loop: true,
+                                }} /></span>
+                        </h1>
                         <p className="mb-5 text-xl">A dedicated frontend developer with a passion for crafting engaging user experiences through the art of coding</p>
                     </div>
                 </div>
