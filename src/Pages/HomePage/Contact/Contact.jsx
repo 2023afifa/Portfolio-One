a024711180de687e4e14a77f4b5041db33ede5b3
afifa <2023afifa@gmail.com>
2024-04-08 14:21:22 +0600

    Contact section style added

--- src/Pages/HomePage/Contact/Contact.jsx
+++ src/Pages/HomePage/Contact/Contact.jsx
@@ -31,7 +31,8 @@ const Contact = () => {
                 <div className="hero-content">
                     <div>
                         <h2 className="text-3xl font-bold mt-10 mb-3 text-center">Contact</h2>
-                        <p className="mb-8 text-center font-semibold">Feel free to reach out — I'd love to connect with you! Looking forward to hearing from you!</p>
+                        <div className="bg-cyan-500 h-2 w-10 rounded-2xl mx-auto"></div>
+                        <p className="mb-8 mt-5 text-center font-semibold">Feel free to reach out — I'd love to connect with you! Looking forward to hearing from you!</p>
                         <div className='flex justify-center' data-aos="fade-up" data-aos-duration="2000">
                             <form ref={form} onSubmit={sendEmail} className='w-2/3 mb-10'>
                                 <label className='mr-2 text-xl'>Name</label><br />
